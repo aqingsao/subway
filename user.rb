8a7f87ab0d08654f284b8546591316db836051b6
aqingsao <vinci.zhang@gmail.com>
2013-08-02 10:20:26 +0800

    introduce logger into user.rb

--- user.rb
+++ user.rb
@@ -1,8 +1,17 @@
+require 'logger'
+LOGGER = Logger.new(STDOUT)
+# LOGGER = Logger.new('subway.log', 'daily')
+LOGGER.datetime_format = "%Y-%m-%d\T%H:%M:%S"
+LOGGER.formatter = proc { |severity, datetime, progname, msg|
+  "#{datetime}: #{msg}\n"
+}
+
 class User
 	def initialize(cardNo)
 		@cardNo = cardNo
 	end
 
 	def visit(from, to)
+		LOGGER.info from
 	end
 end
