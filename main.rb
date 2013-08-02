b16e6be9c5d393d7403af83f3bda70f315c829b7
aqingsao <vinci.zhang@gmail.com>
2013-08-02 14:17:09 +0800

    refactory main.rb

--- main.rb
+++ main.rb
@@ -1,28 +1,31 @@
 require File.join(File.dirname(__FILE__), 'user.rb')
 require File.join(File.dirname(__FILE__), 'subway.rb')
 
-file = "subway.txt"
-
-subway = Subway.new
-newLine = true;
-File.open(file, "r") do |file|  
- 	while str=file.gets
-	 	str.strip!
- 		if(str.empty?)
- 			newLine = true;
- 		else
-	 		if newLine
-	 			newLine = false;
-	 			@line = Line.new(str);
-	 			subway.addLine @line
+def loadSubway(file)
+	subway = Subway.new
+	newLine = true;
+	File.open(file, "r") do |file|  
+	 	while str=file.gets
+		 	str.strip!
+	 		if(str.empty?)
+	 			newLine = true;
 	 		else
-	 			index, name = str.split(" ").collect{|e|e.strip}
-	 			@line.addStation(Station.new(index, name))
+		 		if newLine
+		 			newLine = false;
+		 			@line = Line.new(str);
+		 			subway.addLine @line
+		 		else
+		 			index, name = str.split(" ").collect{|e|e.strip}
+		 			@line.addStation(Station.new(index, name))
+		 		end
 	 		end
- 		end
- 	end 
-end  
+	 	end 
+	end  
+	subway
+end
 
+file = "subway.txt"
+subway = loadSubway(file)
 puts "altogether there are #{subway.lines.length} lines"
 subway.lines.each do |line|
 	puts "#{line.name}"
