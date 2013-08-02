f440d139838ffad01ebe38134b4b2d4b2b1b6719
aqingsao <vinci.zhang@gmail.com>
2013-08-02 14:15:10 +0800

    could load subway model from subway.txt

--- main.rb
+++ main.rb
@@ -3,10 +3,25 @@ require File.join(File.dirname(__FILE__), 'subway.rb')
 
 file = "subway.txt"
 
-subway = Subway.new()
-File.open(file, 'r') do |file|
-
-end
+subway = Subway.new
+newLine = true;
+File.open(file, "r") do |file|  
+ 	while str=file.gets
+	 	str.strip!
+ 		if(str.empty?)
+ 			newLine = true;
+ 		else
+	 		if newLine
+	 			newLine = false;
+	 			@line = Line.new(str);
+	 			subway.addLine @line
+	 		else
+	 			index, name = str.split(" ").collect{|e|e.strip}
+	 			@line.addStation(Station.new(index, name))
+	 		end
+ 		end
+ 	end 
+end  
 
 puts "altogether there are #{subway.lines.length} lines"
 subway.lines.each do |line|
@@ -14,4 +29,5 @@ subway.lines.each do |line|
 	line.stations.each do |station|
 		puts "#{station.index}, #{station.name}"
 	end
+	puts "\n"
 end
