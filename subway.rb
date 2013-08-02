3635a77b1bb5a20204ad59bec2986ae194a54a22
aqingsao <vinci.zhang@gmail.com>
2013-08-02 13:51:30 +0800

    refactor by moving class Subway/Line/Station to file subway.rb

---
+++ subway.rb
@@ -0,0 +1,46 @@
+class Subway
+	attr_reader :lines
+	def initialize()
+		@lines = []
+		@stationIndex = 0
+	end
+	def addLine(line)
+		@lines.push line
+	end
+	def nextStation(stationName)
+		Station.new(getStationIndex(stationName), stationName)
+	end
+	private
+	def getStationIndex(stationName)
+		station = getStationByName(stationName)
+		station ==nil ? @stationIndex+=1 : station.index
+	end
+	def getStationByName(stationName)
+		line = @lines.detect{|line| line.contains(stationName)}
+		line.getStationByName(stationName) unless line.nil?
+	end
+end
+
+class Line
+	attr_reader :name, :stations
+	def initialize(name)
+		@name = name;
+		@stations = []
+	end
+	def addStation(station)
+		@stations.push station
+	end
+	def getStationByName(stationName)
+		@stations.detect {|station| station.name == stationName}
+	end
+	def contains(stationName)
+		@stations.any?{|station| station.name == stationName}
+	end
+end
+
+class Station
+	attr_reader :index, :name
+	def initialize(index, name)
+		@index, @name = index, name
+	end
+end
--- transform.rb
+++ transform.rb
@@ -1,52 +1,7 @@
+require File.join(File.dirname(__FILE__), 'subway.rb')
+
 input = 'original.txt'
 output = 'subway.txt'
-
-INDEX = 0;
-
-class Subway
-	attr_reader :lines
-	def initialize()
-		@lines = []
-		@stationIndex = 0
-	end
-	def addLine(line)
-		@lines.push line
-	end
-	def nextStation(stationName)
-		Station.new(getStationIndex(stationName), stationName)
-	end
-	private
-	def getStationIndex(stationName)
-		station = getStationByName(stationName)
-		station ==nil ? @stationIndex+=1 : station.index
-	end
-	def getStationByName(stationName)
-		line = @lines.detect{|line| line.contains(stationName)}
-		line.getStationByName(stationName) unless line.nil?
-	end
-end
-class Line
-	attr_reader :name, :stations
-	def initialize(name)
-		@name = name;
-		@stations = []
-	end
-	def addStation(station)
-		@stations.push station
-	end
-	def getStationByName(stationName)
-		@stations.detect {|station| station.name == stationName}
-	end
-	def contains(stationName)
-		@stations.any?{|station| station.name == stationName}
-	end
-end
-class Station
-	attr_reader :index, :name
-	def initialize(index, name)
-		@index, @name = index, name
-	end
-end
 
 def parseStation(station)
 	station.split("-").collect{|e|e.strip}
@@ -59,19 +14,18 @@ File.open(input, "r") do |file|
 	 	str.strip!
  		if(str.empty?)
  			newLine = true;
-	 		puts "\n"
  		else
 	 		if newLine
 	 			newLine = false;
 	 			@line = Line.new(str);
 	 			subway.addLine @line
-	 			# puts "line name #{@line.name}"
+	 			puts "line name #{@line.name}"
 	 		else
 	 			stations = str.split("-").collect{|e|e.strip}
 	 			stations.each do |s|
 	 				@line.addStation(subway.nextStation(s))
 	 			end
-	 			# puts "line station #{stations}"
+	 			puts "line station #{stations}"
 	 		end
  		end
  	end 
