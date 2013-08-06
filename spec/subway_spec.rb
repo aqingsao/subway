9f1591ef7bc91b89d677e946751210e9ecdccd80
aqingsao <vinci.zhang@gmail.com>
2013-08-06 10:36:15 +0800

    extract spec file for Station

--- spec/subway_spec.rb
+++ spec/subway_spec.rb
@@ -16,24 +16,6 @@ describe Subway do
 	end
 end
 
-describe Station do
-	before (:each) do
-		@station0 = Station.new(0, "s0")
-		@station1 = Station.new(1, "s1")
-	end
-	describe 'eql?' do
-		it 'return true when obj compares with itself' do
-			expect(@station0.eql? @station0).to be_true
-		end
-		it 'return true when obj compares with other which has same name and index' do
-			expect(@station0.eql? Station.new(0, "s0")).to be_true
-		end
-		it 'return false when obj compares with other which has different name or index' do
-			expect(@station0.eql? @station1).to be_false
-		end
-	end
-end
-
 describe Edge do
 	before (:each) do
 		@station0 = Station.new(0, "s0")
