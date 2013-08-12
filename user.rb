9ef5003564cb120b356b5076f7ca493a46433348
aqingsao <vinci.zhang@gmail.com>
2013-08-12 17:16:39 +0800

    rename data1 to group1

--- user.rb
+++ user.rb
@@ -1,6 +1,6 @@
 require 'logger'
 LOGGER = Logger.new(STDOUT)
-LOGGER = Logger.new('data1/subway.log', 'daily')
+LOGGER = Logger.new('group1/production.log', 'daily')
 LOGGER.datetime_format = "%Y-%m-%d\T%H:%M:%S"
 LOGGER.formatter = proc { |severity, datetime, progname, msg|
   "#{datetime}: #{msg}\n"
